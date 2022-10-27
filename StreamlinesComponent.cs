a666bdbe54e4c6acb76825f80f60a6e0a730d651
mariuszhermansdorfer <mrhe@ramboll.com>
2022-10-27 17:48:06 +0200

    Add selection

--- StreamlinesComponent.cs
+++ StreamlinesComponent.cs
@@ -21,16 +21,22 @@ namespace Streamlines
 
         private int _skip;
         private int _spacing;
-
+        private Box _selectionBox;
+        private bool _reset;
+        
+        private RTree _tree;
         private PointCloud _probes;
         private List<Vector3d[]> _frames;
+        private List<int> _startingPointsIds;
         private Polyline[] _streamlines;
-        private List<double>[] _velocities;
+        private List<double>[] _velocityMagnitudes;
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddIntegerParameter("spacing", "spacing", "", GH_ParamAccess.item);
             pManager.AddIntegerParameter("skip", "skip", "", GH_ParamAccess.item);
+            pManager.AddBoxParameter("selectionBox", "selectionBox", "", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("reset", "reset", "", GH_ParamAccess.item, _reset);
         }
 
 
@@ -50,7 +56,18 @@ namespace Streamlines
             if (!DA.GetData(1, ref _skip))
                 return;
 
+            if (!DA.GetData(2, ref _selectionBox))
+                return;
+
+            DA.GetData(3, ref _reset);
+
+            if (_reset)
+                _tree = null;
+
             _probes = Core.CreateProbePoints(_spacing);
+            if (_tree == null)
+                _tree = RTree.CreatePointCloudTree(_probes);
+
             var clippingBox = _probes.GetBoundingBox(false);
 
             _frames = new List<Vector3d[]>();
@@ -58,20 +75,33 @@ namespace Streamlines
                 _frames.Add(Core.CreateVelocityVectors(_spacing));
 
 
-            _streamlines = new Polyline[_spacing * _spacing]; // TODO adapt to wind tunnel dimensions
-            _velocities = new List<double>[_spacing * _spacing];
+            _startingPointsIds = new List<int>();
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            _tree.Search(_selectionBox.BoundingBox, SearchCallback);
+            Rhino.RhinoApp.WriteLine($"Search {sw.ElapsedMilliseconds} ms");
+            sw.Stop();
+
+            _streamlines = new Polyline[_startingPointsIds.Count];
+            _velocityMagnitudes = new List<double>[_startingPointsIds.Count];
+
+            for (int i = 0; i < _startingPointsIds.Count; i++)
+            {
+                int currentPoint = _startingPointsIds[i];
+                _streamlines[i] = new Polyline();
+                _streamlines[i].Add(_probes[currentPoint].Location);
+
+                _velocityMagnitudes[i] = new List<double>();
+                _velocityMagnitudes[i].Add(_frames[0][currentPoint].Length);
+            }
 
 
+
+            sw.Restart();
             Parallel.For(0, (_streamlines.Length - 1) / _skip, i =>
             //for (int i = 0; i < _streamlines.Length - 1; i += _skip)
             {
                 i *= _skip; // Only needed for the Parallel.For loop
-
-                // Create streamlines and assign their initial positions
-                _streamlines[i] = new Polyline();
-                _streamlines[i].Add(_probes[i].Location);
-                _velocities[i] = new List<double>();
-                _velocities[i].Add(_frames[0][i].Length);
 
                 // Iterate over frames
                 for (int j = 0; j < _frames.Count - 1; j++)
@@ -84,12 +114,14 @@ namespace Streamlines
                     var currentVelocity = _frames[j][closestProbe];
                     var destination = currentLocation + currentVelocity;
                     _streamlines[i].Add(destination);
-                    _velocities[i].Add(currentVelocity.Length);
+                    _velocityMagnitudes[i].Add(currentVelocity.Length);
                 }
             });
-
+            Rhino.RhinoApp.WriteLine($"Parallel For {sw.ElapsedMilliseconds} ms");
             var vertexBuffer = new DataTree<GH_Point>();
             var vertexColorBuffer = new DataTree<double>();
+            
+            sw.Restart();
 
             for (int i = 0; i < _streamlines.Length; i++)
                 if (_streamlines[i] == null)
@@ -100,16 +132,20 @@ namespace Streamlines
                     for (int j = 0; j < _streamlines[i].Count; j++)
                     {
                         vertexBuffer.Add(new GH_Point(_streamlines[i][j]), path);
-                        vertexColorBuffer.Add(_velocities[i][j], path);
+                        vertexColorBuffer.Add(_velocityMagnitudes[i][j], path);
                     }
                 }
-
+            Rhino.RhinoApp.WriteLine($"For {sw.ElapsedMilliseconds} ms");
 
             DA.SetDataTree(0, vertexBuffer);
             DA.SetDataTree(1, vertexColorBuffer);
 
         }
 
+        private void SearchCallback(object sender, RTreeEventArgs e)
+        {
+            _startingPointsIds.Add(e.Id);
+        }
         protected override System.Drawing.Bitmap Icon => null;
 
 
